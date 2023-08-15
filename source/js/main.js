07d9302c2d836413d5f116446c6e8d0667949613
Alina <alina.melikhova@gmail.com>
2023-08-15 17:54:16 +0300

    adds tours section, starts education

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import { iosVhFix } from "./utils/ios-vh-fix";
 import { initModals } from "./modules/modals/init-modals";
 import { Form } from "./modules/form-validate/form";
 import { handleMobileMenu } from "./modules/mobile-menu";
+import { playVideo } from "./modules/youtube";
 
 // ---------------------------------
 
@@ -13,6 +14,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // Modules
   handleMobileMenu();
+  playVideo();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
--- source/js/modules/mobile-menu.js
+++ source/js/modules/mobile-menu.js
@@ -2,9 +2,13 @@ const menuToggleButton = document.querySelector(".header__mob-menu-button");
 const menuOpenIcon = document.querySelector(".header__mob-menu-icon-burger");
 const menuCloseIcon = document.querySelector(".header__mob-menu-icon-cross");
 const menuWrapper = document.querySelector(".header__positioning-wrapper");
+const pageWrapper = document.querySelector(".page-wrapper");
+const heroWrapper = document.querySelector(".hero__container");
 
 export const handleMobileMenu = () => {
   menuToggleButton.addEventListener("click", () => {
+    pageWrapper.classList.toggle("page-wrapper--mob-open");
+    heroWrapper.classList.toggle("hero__container--mob-open");
     menuWrapper.classList.toggle("header__positioning-wrapper--mob-open");
     menuOpenIcon.classList.toggle("header__mob-menu-icon-burger--closed");
     menuCloseIcon.classList.toggle("header__mob-menu-icon-cross--opened");
